6af196437f25ec8bbd33895478ad0f5622e1e674
kamil wojciechowski <kamil@trade.io>
2019-04-03 22:44:15 +0200

    fix

--- src/XsgTwitterBot/AppModule.cs
+++ src/XsgTwitterBot/AppModule.cs
@@ -17,8 +17,9 @@ namespace XsgTwitterBot
 
             builder.RegisterType<ExplorerApi>().AsImplementedInterfaces().InstancePerDependency();
             builder.RegisterType<SyncCheckService>().AsImplementedInterfaces().InstancePerDependency();
-
-            builder.Register(container => new LiteDatabase("rewards.db")).SingleInstance();
+            
+            var dbPath = Path.Combine("db", Path.PathSeparator.ToString(), "rewards.db");
+            builder.Register(container => new LiteDatabase(dbPath)).SingleInstance();
             builder.Register(container => container.Resolve<LiteDatabase>().GetCollection<Reward>("rewards")).SingleInstance();
             builder.RegisterType<BotEngine>().SingleInstance();
         }
